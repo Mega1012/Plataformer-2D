79aa9ab0662170b227261444e5406a91de165b6e
Mega1012 <felipe.fsp303@gmail.com>
2024-01-24 07:46:35 -0300

    prototype player jump/movement and EnemyBase/Health

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+    [Header("Speed Setup")]
     public Rigidbody2D myRigidbody;
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
@@ -11,10 +13,18 @@ public class Player : MonoBehaviour
     public float speedrun;
     public float _currentSpeed;
 
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.7f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
+
+
     private void Update()
     {
         HandleMoviment();
         HandleJump();
+        
     }
 
     private void HandleMoviment()
@@ -53,7 +63,20 @@ public class Player : MonoBehaviour
     private void HandleJump()
     {
         if (Input.GetKey(KeyCode.Space))
+        {
             myRigidbody.velocity = Vector2.up * forcejump;
+            myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
+            HandleScaleJump();
+        }
+    }
+
+
+    private void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
 
     }
 
