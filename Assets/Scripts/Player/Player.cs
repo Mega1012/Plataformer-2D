8ac710c2137d94edf03ef6ddeabd87067e9803bb
Mega1012 <felipe.fsp303@gmail.com>
2024-02-02 08:20:31 -0300

    add vfx jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -16,6 +16,13 @@ public class Player : MonoBehaviour
 
     public Animator _currentPlayer;
 
+    [Header("Jump Collision Check")]
+    public Collider2D collider2D;
+    public float distToGround;
+    public float spaceToGround = .1f;
+    public ParticleSystem jumpVFX;
+
+
     private void Awake()
     {
         if(healthBase != null)
@@ -24,6 +31,18 @@ public class Player : MonoBehaviour
         }
 
         _currentPlayer = Instantiate(SOPlayerSetup.player, transform);
+
+        if(collider2D != null)
+        {
+            distToGround = collider2D.bounds.extents.y;
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, distToGround + distToGround);
+        return Physics2D.Raycast(transform.position, -Vector2.up, distToGround + spaceToGround);
     }
 
     private void OnPlayerKill()
@@ -34,6 +53,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        IsGrounded();
         HandleMoviment();
         HandleJump();
         
@@ -87,14 +107,20 @@ public class Player : MonoBehaviour
 
     private void HandleJump()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
         {
             myRigidbody.velocity = Vector2.up * SOPlayerSetup.forcejump;
             myRigidbody.transform.localScale = Vector2.one;
 
             DOTween.Kill(myRigidbody.transform);
             HandleScaleJump();
+            PlayJumpVFX();
         }
+    }
+
+    private void PlayJumpVFX()
+    {
+        if (jumpVFX != null) jumpVFX.Play();
     }
 
 
